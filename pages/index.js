c2a3ecf38eb9cedd95a106a6b9f17d5de4138fda
fabiovaz81 <42327504+fabiovaz81@users.noreply.github.com>
2021-01-30 11:42:00 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
 import styled from 'styled-components'
+import db from '../db.json';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz do mengão</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Teste seus conhecimentos sobre o Mais Querido!</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+
+            <p>Teste Teste Teste Teste</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+        <GitHubCorner projectUrl="https://www.github.com/fabiovaz81"/>
+
+      </QuizContainer>
+    </QuizBackground>
+  );
 }
