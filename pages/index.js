40d9ac89a019d054585ad00f2e6f1c9b89fa4df6
fabiovaz81 <42327504+fabiovaz81@users.noreply.github.com>
2021-01-30 13:12:08 -0300

    eslint, quizPage e submit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import styled from 'styled-components'
-import db from '../db.json';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
@@ -18,16 +21,41 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
 
     <QuizBackground backgroundImage={db.bg}>
+
+      <Head>
+        <title>MengãoQuiz - O Mais Querido</title>
+      </Head>
+
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>Quiz do mengão</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Teste seus conhecimentos sobre o Mais Querido!</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('submit do react');
+
+              // router manda para a próxima tela
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Digite seu nome" />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -40,7 +68,7 @@ export default function Home() {
         </Widget>
 
         <Footer />
-        <GitHubCorner projectUrl="https://www.github.com/fabiovaz81"/>
+        <GitHubCorner projectUrl="https://www.github.com/fabiovaz81" />
 
       </QuizContainer>
     </QuizBackground>
